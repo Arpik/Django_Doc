b9c783695b5daba65c46c0921636aacb440947d4
Arpik <arpiksargsyan@gmail.com>
2019-09-24 17:21:42 +0400

    Added __str__() method to Choice class

--- Django_Doc/polls/models.py
+++ Django_Doc/polls/models.py
@@ -6,10 +6,14 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
-        return self.self.question_text
+        return self.question_text
     
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length = 200)
     votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.choice_text
+    
