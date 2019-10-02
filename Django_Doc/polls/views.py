8f8e478227b56d446b2ac5d09aa6bba4cbcf3c65
Arpik <arpiksargsyan@gmail.com>
2019-10-02 18:04:56 +0400

    Replaced index view with IndexView generic view.

--- Django_Doc/polls/views.py
+++ Django_Doc/polls/views.py
@@ -5,11 +5,13 @@ from django.urls import reverse
 
 from .models import Choice, Question
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list' : latest_question_list,}
-    return render(request, 'polls/index.html', context)
+def IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[5] 
+    
 def detail(request, question_id):
     question = get_object_or_404(Question, pk = question_id)
     return render(request, 'polls/detail.html', {'question':question})
