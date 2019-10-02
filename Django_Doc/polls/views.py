d6933023566d0f946cbf502567988eec481f6a65
Arpik <arpiksargsyan@gmail.com>
2019-10-02 18:09:19 +0400

    Replaced results view with ResultsView generic view

--- Django_Doc/polls/views.py
+++ Django_Doc/polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-
+from django.views import generic 
 
 from .models import Choice, Question
 
@@ -16,9 +16,9 @@ class DetailView(generic.DetailView):
     model = Question
     template_name= 'polls/detail.html' 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
