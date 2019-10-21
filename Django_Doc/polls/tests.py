ce0682ba14bdb42091887cb37547a81714c91992
Arpik <arpiksargsyan@gmail.com>
2019-10-21 17:08:00 +0400

    Added test_no_quesiton() function which checks the message and verifies the latest_quesiton_list is empty.

--- Django_Doc/polls/tests.py
+++ Django_Doc/polls/tests.py
@@ -42,3 +42,15 @@ class QuestionModelTests(TestCase):
         """
         time = timezone.now() + datetime.timedelta(days = days)
         return Question.objects.create(question_text = question_text, pub_date = time)
+    
+    class QesutionIndexViewTests(TestCase):
+        def test_no_question(self):
+            """
+                If no question exist, an appropiriate message is displayed.
+            """
+            response = self.client.get(reverse('polls:index'))
+            self.assertEqual(response.status_code, 200)
+            self.assertContains(response, "No polls are available.")
+            self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+     
